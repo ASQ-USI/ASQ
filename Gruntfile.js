6fc80a8583d9ce8457c867a87254c230a5d6b103
Vasileios Triglianos <triglian@gmail.com>
2013-07-31 18:24:47 +0200

    add more options to grunt file

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,72 +1,86 @@
-var mongoose        = require('mongoose')
-  
-
 module.exports = function(grunt) {
+  'use strict';
+
+  grunt.loadNpmTasks('grunt-mocha-test');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-concat');
 
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+
+    //mocha tests
+    mochaTest: {
+      test: {
+        options: {
+          reporter: 'spec'
+        },
+        src: ['test/**/*.js']
+      }
+    },
+
+    jshint: {
+      all: ['Gruntfile.js']//, 'public/js/**/*.js', 'test/**/*.js']
+    },
+
+    //concat
+    concat: {
+      options: {
+        separator: ';'
+      },
+      dist: {
+        src: ['public/js/**/*.js'],
+        dest: 'build/asq.js'
+      }
+    },
+
+    //uglify
     uglify: {
       options: {
+        screw_ie8 : true,
         banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
       },
       build: {
-        src: 'src/<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
+        files: {
+          'build/asq.min.js' : ['build/asq.js']
+        }
       }
+    }
+
+  });
+
+  // Default task(s).
+  grunt.registerTask('default', ['mochaTest', 'jshint', 'concat', 'uglify']);
+
+
+  grunt.registerTask('db', "Tasks for the Mongo Database", function(command) {
+    console.log(process.env);
+    switch (command) {
+      case "drop" :
+        grunt.task.run('dbDrop');
+        break;
     }
   });
 
-  // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.registerTask('dbDrop', "Drops the schema of the asq database", function() {
+    var mongoose = require('mongoose')
+    , done = this.async();
 
-  // Default task(s).
-  grunt.registerTask('default', ['uglify']);
-  grunt.registerTask('db', "Tasks for the Mongo Database", function(command){
-    switch(command){
-      case : "drop"
-        grunt.run('dbDrop')
-    }
-  })
-
-  grunt.registerTask('dbDrop', "Drops the schema of the asq database", function(){
-    var mongoose = require('mongoose');
-    mongoose.connect('mongo://localhost/mydatabase', function(err) {
-      mongoose.connection.db.dropDatabase();
+    mongoose.connect('mongodb://localhost:27017/asq', function(err) {
+      if (err) {
+        throw err;
+      }
+      grunt.log.writeln('Dropping database: asq ...');
+      mongoose.connection.db.dropDatabase(function(err) {
+        if (err) {
+          done(err);
+        } else {
+          grunt.log.ok();
+          done();
+        }
+      });
     });
-  })
+  });
 
 };
-
-
-// var mongoose        = require('mongoose')
-// , slideshowSchema   = require('./models/slideshow')         
-
-// namespace('db', function () {
-//   desc('This the db:emptySlideshows task');
-//   task('emptySlideshows', [], function () {
-//     console.log('doing db:emptySlideshows task');
-//    // console.log(sys.inspect(arguments));
-
-//     var db = mongoose.createConnection('127.0.0.1', 'asq')
-//     , Slideshow = db.model('Slideshow');
-
-//      Slideshow.remove({}, function(err){
-//       console.log("I am in")
-//         complete();
-//       if(err){
-//         console.log(err)
-//       }
-//        complete();
-//     });
-
-     
-//   }, true);
-
-//   desc('This the db:emptyQuestions task');
-//   task('emptyQuestion', [], function () {
-//     console.log('doing foo:baz task');
-//   //  console.log(sys.inspect(arguments));
-//   });
-
-// });
