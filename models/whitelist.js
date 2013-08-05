b91aaead16e44f08895d866f8fa8061dbe438dab
Jacques DAFFLON <jacques.dafflon@usi.ch>
2013-08-05 13:18:17 +0200

    Updated whitelist

--- models/whitelist.js
+++ models/whitelist.js
@@ -2,28 +2,16 @@ var mongoose  = require('mongoose')
 , Schema      = mongoose.Schema
 , ObjectId    = Schema.ObjectId;
 
-
-var anonymousWhitelistSchema = new Schema({
+var whitelistEntrySchema = new Schema({
 	session : { type: ObjectId, ref: 'Session', required: true },
 	uid : { type: ObjectId, ref: 'User', required: true },
-	token : {type: String, required: true }
-}, {collection: "whitelistEntries" });
+	token : { type: String } // Express Cookie session id
+}, { collection: "whitelistEntries" });
 
-anonymousWhitelistSchema.index({ session: 1, uid: 1});
+whitelistEntrySchema.index({ session: 1, uid: 1});
 
-mongoose.model("AnonymousWhitelist", anonymousWhitelistSchema);
-
-var privateWhitelistSchema = new Schema({
-	session : { type: ObjectId, ref: 'Session', required: true },
-	uid : { type: ObjectId, ref: 'User', required: true }
-}, {collection: "whitelistEntries" });
-
-privateWhitelistSchema.index({ session: 1, uid: 1});
-
-mongoose.model("PrivateWhitelist", privateWhitelistSchema);
+mongoose.model("WhitelistEntry", whitelistEntrySchema);
 
 module.exports = {
-	anonymousWhitelistSchema : anonymousWhitelistSchema,
-	privateWhitelistSchema : privateWhitelistSchema
+	whitelistEntrySchema : whitelistEntrySchema
 }
-
