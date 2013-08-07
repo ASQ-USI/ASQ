44e1afcbe0ad86764aa51a17bf247018f983f2e7
Jacques DAFFLON <jacques.dafflon@usi.ch>
2013-08-07 14:53:46 +0200

    Added display name and code cleaning

--- models/whitelist.js
+++ models/whitelist.js
@@ -6,10 +6,11 @@ var whitelistEntrySchema = new Schema({
 	session : { type: ObjectId, ref: 'Session', required: true },
 	uid : { type: ObjectId, ref: 'User', required: true },
 	token : { type: String }, // Express Cookie session id
+	displayName : { type: String },
 	canControl : { type: Boolean, default: false }
 }, { collection: "whitelistEntries" });
 
-whitelistEntrySchema.index({ session: 1, uid: 1});
+whitelistEntrySchema.index({ session: 1, uid: 1 });
 
 mongoose.model("WhitelistEntry", whitelistEntrySchema);
 
