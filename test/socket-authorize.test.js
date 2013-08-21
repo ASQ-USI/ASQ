29485589083847aee50fb17b3b94ac8f7871983e
Jacques DAFFLON <jacques.dafflon@usi.ch>
2013-08-21 17:08:19 +0200

    Fixed test, now working!

--- test/socket-authorize.test.js
+++ test/socket-authorize.test.js
@@ -15,127 +15,150 @@ schemas = require('../models')
 db = mongoose.createConnection(config.mongoDBServer, config.dbName,
         config.mongoDBPort);
 
+// Create the session
+var testSession = null;
+var sessionId = mongoose.Types.ObjectId('4edd40c86762e0fb12000003');
 
-describe('Socket ctrl authorization', function testCtrlAuth() {
-  // Create the session Id
-  var sessionId = mongoose.Types.ObjectId('4edd40c86762e0fb12000003');
-  // Create the whitelist data
-  var token = 'test token';
-  var displayName = 'test name';
-  // Create the handhsake data
-    var handshakeData = {
-      "query" : {
-        "sid" : sessionId
-      },
-      "headers" : {
-        "cookie" : token
+// Create the whitelist
+var testWhitelistEntry = null;
+var token = 'test token';
+var displayName = 'test name';
+
+// Create the handhsake data
+var handshakeData = {
+  "query" : {
+    "sid" : sessionId
+  },
+  "headers" : {
+    "cookie" : token
+  }
+};
+
+before(function(done) {
+
+  function populateDB() {
+    // Create a Session
+    var Session = db.model('Session', schemas.sessionSchema);
+    testSession = new Session({
+      _id : sessionId
+    });
+
+    // Create a whitelist entry
+    var WhitelistEntry = db.model('WhitelistEntry', schemas.whitelistEntrySchema);
+    testWhitelistEntry = new WhitelistEntry({
+      session     : sessionId,
+      uid         : mongoose.Types.ObjectId('4edd40c86762e0fb12000003'),
+      token       : token,
+      displayName : displayName,
+      canControl  : true
+    });
+    testSession.save(function(err, session) {
+      if (err) {
+        done(err);
       }
-    };
+      testWhitelistEntry.save(done);
+    });
+  }
+  populateDB();
+});
 
-  before(function(done) {
+describe('Test ctrlAuthorize', function testCtrlAuth() {
 
-    function populateDB() {
-      // Create a Session
-      var Session = db.model('Session', schemas.sessionSchema);
-      var testSession = new Session({
-        _id : sessionId
+  describe('with an empty handshake', function(){
+    var error, isAuthorized;
+
+    before(function(done){
+      authentication.ctrlAuthorize({}, function(err, authorized) {
+        error = err;
+        isAuthorized = authorized;
+        done();
+      });
+    });
+
+    describe('Refuse the connection with an error message', function() {
+      it('Should return an error saying "Missing session".', function() {
+        expect(error).to.deep.equal(new Error('Missing session'));
       });
 
-      // Create a whitelist entry
-      var WhitelistEntry = db.model('WhitelistEntry', schemas.whitelistEntrySchema);
-      var testWhitelistEntry = new WhitelistEntry({
-        session     : sessionId,
-        uid         : mongoose.Types.ObjectId('4edd40c86762e0fb12000003'),
-        token       : token,
-        displayName : displayName
-      });
-      testSession.save(function(err, session) {
-        if (err) {
-          done(err);
-        }
-        testWhitelistEntry.save(done);
-      });
-    }
-    populateDB();
-  });
-
-  describe('Empty handshake', function(){
-    authentication.ctrlAuthorize({}, function(err, authorized) {
-      it('Should return an error saying missing session', function() {
-        expect(err).to.equal('Missing session');
-      });
-
-      it('Should not authorize the connection', function() {
-        expect(authorized).to.equal(false);
+      it('Should not authorize the connection.', function() {
+        expect(isAuthorized).to.be.false;
       });
     });
   });
 
-  describe('Empty query', function() {
-    authentication.ctrlAuthorize({ "query" : {} }, function(err, authorized) {
-      it('Should return an error saying missing session', function() {
-        expect(err).to.equal('Missing session');
+  describe('with an empty query', function() {
+    var error, isAuthorized;
+
+    before(function(done){
+      authentication.ctrlAuthorize({ "query" : {} }, function(err, authorized) {
+        error = err;
+        isAuthorized = authorized;
+        done();
+      });
+    });
+
+    describe('Refuse the connection with an error message', function() {
+      it('Should return an error saying "Missing session".', function() {
+        expect(error).to.deep.equal(new Error('Missing session'));
       });
 
-      it('Should not authorize the connection', function() {
-        expect(authorized).to.equal(false);
+      it('Should not authorize the connection.', function() {
+        expect(isAuthorized).to.be.false;
       });
     });
   });
 
   describe('Valid connection', function() {
-    var err, authorized;
+    var error, isAuthorized;
+    var sessionKeysToTest = ['_id', 'startDate', 'activeSlide', 'authLevel'];
 
-    // if we want multiple tests on a callback it's more clean to do it like this
-  // (with a before close)cat
-    // I know I had like you did, but apparently we have silent fails of callbacks
-    // so I need to change it too
-
-    // This never enters the callback
     before(function(done){
       authentication.ctrlAuthorize(handshakeData, function(err, authorized) {
-        // we cannot reach here
-        console.log("I am in")
-        err=err;
-        console.log(err)
-        authorized=authorized;
+        error = err;
+        isAuthorized = authorized;
         done();
       });
-    })
+    });
 
-    it('Should not return an error', function() {
-      expect(err).to.equal(null);
-    });
-    it('Should authorize the connection', function() {
-      expect(authorized).to.equal(true);
-    });
-    it('Should set the session in the handshake', function() {
-      expect(handshakeData.session).to.deep.equals(testSession);
-    });
-    it('Should set the displayName in the handshake', function() {
-      expect(handshakeData.displayName).to.equal(dispayName);
+    describe('Accept the connection and update the handshake', function() {
+      it('Should not return an error', function() {
+        expect(error).to.be.null;
+      });
+      it('Should authorize the connection', function() {
+        expect(isAuthorized).to.equal(true);
+      });
+      it('Should set the session in the handshake', function() {
+        for (var i in sessionKeysToTest) {
+          var key = sessionKeysToTest[i];
+          expect(handshakeData.session[key]).to.deep.equal(testSession[key]);
+          // For some reason to.have(.deep).property(key, value) does not work.
+        }
+      });
+      it('Should set the displayName in the handshake', function() {
+        expect(handshakeData.displayName).to.equal(displayName);
+      });
     });
   });
 
+});
 
-  afterEach(function (done) {
-    var Session = db.model('Session', schemas.sessionSchema);
-    var WhitelistEntry = db.model('WhitelistEntry', schemas.whitelistEntrySchema);
+after(function (done) {
+  var Session = db.model('Session', schemas.sessionSchema);
+  var WhitelistEntry = db.model('WhitelistEntry', schemas.whitelistEntrySchema);
 
-    Session.remove({ _id : sessionId}, function(err) {
+  Session.remove({ _id : sessionId}, function(err) {
+    if (err) {
+      return done(err);
+    }
+    WhitelistEntry.remove({ session : sessionId }, function(err) {
       if (err) {
         return done(err);
       }
-      WhitelistEntry.remove({ session : sessionId }, function(err) {
-        if (err) {
-          return done(err);
-        }
-        mongoose.disconnect();
-      });
-    })
-    
-    return done();
-  });
+      mongoose.disconnect();
+    });
+  })
+  
+  return done();
 });
 
 
