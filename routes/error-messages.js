58225b8e8e68637feeceef8020e17901228c2ad3
Jacques DAFFLON <jacques.dafflon@usi.ch>
2013-09-04 16:58:53 +0200

    Factored error messages out of utils

---
+++ routes/error-messages.js
@@ -0,0 +1,27 @@
+
+module.exports = errors = {};
+
+errors.username = {
+  notEmpty : 'Username missing.',
+  notNull  : 'Username missing.',
+  regex    : 'Allowed: 3 to 12 characters among letters, digit or . - _.'
+};
+
+errors.email = {
+  notEmpty : 'Email missing.',
+  notNull  : 'Email missing.',
+  len      : 'The email should between 6 and 64 characters long.',
+  isEmail  : 'This email is invalid.'
+};
+
+errors.password = {
+  notEmpty : 'Password missing',
+  notNull  : 'Password missing',
+  regex    : ['Allowed: 8 to 30 characters, at least a lower and a an upper ',
+              'case characters and a digit, the following characters are ',
+              'allowed: ! @ # % : _ ( ) $ ^ & * - . ?'].join('')
+}
+
+errors.passwordConfirm = {
+  equals : 'Passwords are not matching.'
+}
