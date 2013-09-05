1a9b83b81827777b553ca0625342051e909add74
Jacques DAFFLON <jacques.dafflon@usi.ch>
2013-09-05 12:44:13 +0200

    Added logging and some bug fixing

--- routes/user/routes.js
+++ routes/user/routes.js
@@ -1,15 +1,17 @@
 var handler       = require('./handler')
-  , presentations = require('./presentations');
+  , presentations = require('./presentations')
+  , appLogger     = require('../../lib/logger').appLogger;
 
-function setUp(app, middleware) {
+module.exports.setUp = function setUp(app, middleware) {
+  appLogger.debug('Setting user routes');
   //Render the user page
-  app.get('/:user/' handler.getUserPage);
+  app.get('/:user/', handler.getUserPage);
   
   //Render the user settings
   app.get('/:user/settings/', middleware.isRouteOwner, handler.getUserSettings);
   
   //Update the user settings
-  app.put('/:user/settings', middleware,isRouteOwner,
+  app.put('/:user/settings', middleware.isRouteOwner,
     handler.updateUserSettings);
 
   presentations.setUp(app, middleware);
