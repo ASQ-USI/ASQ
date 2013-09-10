c18f3222d98fa0db9ce88a728fa6eabb86c4d3e7
Jacques DAFFLON <jacques.dafflon@usi.ch>
2013-09-10 14:41:11 +0200

    Fixing middlewares order

--- app.js
+++ app.js
@@ -268,7 +268,7 @@ app.get('/checkusername/:username/', registration.checkusername);
 //MOVED
 //app.get('/slides/thumbs/:id/:file', ensureAuthenticated, routes.slides.serveThumbs)
 
-//REDIRECT LOOP FOR SOME REASON
+app.get('/user/edit/', ensureAuthenticated, function (req,res) {
 // app.get('/user/edit/', ensureAuthenticated, function (req,res) {
 //     res.redirect("/user/edit")
 // });
