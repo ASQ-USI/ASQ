f3643516883de2e4bf4d572c29f6d9bb253ca443
Jacques DAFFLON <jacques.dafflon@usi.ch>
2013-09-12 15:59:13 +0200

    Added forms for post in the presentations page to start a new broadcast

--- routes/user/presentations/routes.js
+++ routes/user/presentations/routes.js
@@ -1,4 +1,4 @@
-var handler      = require('./handler')
+var handlers      = require('./handlers')
   , presentation = require('./presentation')
   , appLogger    = require('../../../lib/logger').appLogger;
 
@@ -7,29 +7,29 @@ module.exports.setUp = function setUp(app, middleware) {
 
   // List all the user's presentations.
   app.get('/:user/presentations/', middleware.isRouteOwner,
-    handler.listPresentations);
+    handlers.listPresentations);
   
   // Upload a new presentation.
   app.post('/:user/presentations', middleware.isRouteOwner,
-    handler.uploadPresentation);
+    handlers.uploadPresentation);
 
   // Update the presentation matching presentationId.
   app.put('/:user/presentations/:presentationId', middleware.isRouteOwner,
-      handler.updatePresentation);
+      handlers.updatePresentation);
 
   // Delete the presentation matching presentationId.
   app.del('/:user/presentations/:presentationId', middleware.isRouteOwner,
-      handler.deletePresentation);
+      handlers.deletePresentation);
 
   // Get the presentation matching presentationId.
   app.get('/:user/presentations/:presentationId/', middleware.isRouteOwner,
-      handler.getPresentation);
+      handlers.getPresentation);
 
   // Set up presentation specific routes
   // before the catchall (*) four lines below.
   presentation.setUp(app, middleware);
 
   // Serve static files attached to the slideshow
-  app.get('/:user/presentations/:presentationId/*', middleware.isRouteOwner,
-      handler.getPresentationFiles);
+  // app.get('/:user/presentations/:presentationId/?!(edit|live|start)',
+  //   middleware.isRouteOwner, handlers.getPresentationFiles);
 }
