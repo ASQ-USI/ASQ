f0dcdb8d1f16472885ab32c04cc7035ec0a4aee5
Vasileios Triglianos <triglian@gmail.com>
2013-09-17 02:25:17 +0200

    Add: db:empty task Grunt task

--- tasks/db.js
+++ tasks/db.js
@@ -4,13 +4,41 @@
 
 'use strict';
 
+var mongoose = require('mongoose');
+
 module.exports = function(grunt) {
 
   //the basic db task. It calls all the other tasks
-  grunt.registerTask('db', "Tasks for the Mongo Database", function(command) {
+  grunt.registerTask('db', "Tasks for the Mongo Database", function(command, collection) {
+    // see if collection is specified
+    var subtask = typeof collection == 'undefined' ? '' : (':' + collection)
+
+    //set environment
+    global['env'] = grunt.option('env') || 'development';
+
+    // figure which database to act upon
+    switch (env) {
+      case "dev" :
+      case "development" :
+        global['dbName'] = "asq-dev";
+        break;
+      case "test" :
+        global['dbName'] = "asq-test";
+        break;
+      default:
+        global['dbName'] = "asq-dev";
+    }
+
+    //run the actual task
     switch (command) {
       case "drop" :
         grunt.task.run('dbDrop');
+        break;
+      case "empty" :
+        if(subtask ==''){
+          grunt.log.error('"empty" target needs a valid collection (model) argument');
+        }
+        grunt.task.run('dbEmpty'+ subtask);
         break;
     }
   });
@@ -18,21 +46,8 @@ module.exports = function(grunt) {
   //This task is responsible for droping the databases
   grunt.registerTask('dbDrop', "Drops the schema of the asq database", function() {
     var done = this.async()
-    , mongoose = require('mongoose')
-    , env = grunt.option('env') || 'development'
-    , dbName;
-
-    switch (env) {
-      case "dev" :
-      case "development" :
-        dbName = "asq-dev";
-        break;
-      case "test" :
-        dbName = "asq-test";
-        break;
-      default:
-        dbName = "asq-dev";
-    }
+    , dbName =  global['dbName'];
+    
 
     mongoose.connect('mongodb://localhost:27017/'+dbName, function(err) {
       if (err) {
@@ -49,4 +64,23 @@ module.exports = function(grunt) {
       });
     });
   });
+
+    //This task is responsible for droping the databases
+  grunt.registerTask('dbEmpty', "Empty all or a specific collection from the db", function(collection) {
+    var done = this.async()
+      , dbName =  global['dbName']
+      global.db = mongoose.createConnection('mongodb://localhost:27017/'+dbName)
+      var schemas = require('../models');
+
+    grunt.log.writeln('Emptying model: '+ dbName +'.' + collection + ' ...');
+
+    var TheModel = db.model(collection);
+    TheModel.remove({},function (err) {
+      if(err) done(err);
+      grunt.log.ok();
+      done();
+    });
+
+  });
+
 };
