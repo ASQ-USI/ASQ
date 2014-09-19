f92293326f03bfa207ef6a77e53a4619288fed1b
Vasileios Triglianos <triglian@gmail.com>
2014-09-19 13:12:15 +0200

    remove console.log

--- routes/user/presentations/handlers.js
+++ routes/user/presentations/handlers.js
@@ -171,7 +171,6 @@ function uploadPresentation(req, res, next) {
         slideShowFileHtml = replaceAll('{','ESCAPEFORDUSTBRACKETSASQ',slideShowFileHtml)
         slideShowFileHtml = replaceAll('}','{~rb}',slideShowFileHtml)
         slideShowFileHtml = replaceAll('ESCAPEFORDUSTBRACKETSASQ','{~lb}',slideShowFileHtml)
-       // console.log(slideShowFileHtml)
 
         appLogger.debug('parsing main .html file for questions...');
         return (new Parser(appLogger)).parse(slideShowFileHtml, { wrap: false });
