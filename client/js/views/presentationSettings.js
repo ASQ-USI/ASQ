5b7539a43a897def363bead1960e4e87699b028b
ZhenfeiNie <zhenfei.nie@gmail.com>
2015-06-30 15:28:56 +0200

    client/js: move client/js/presentationSettingsBindings.js to presentationSettings; add logics to update slideshow settings

---
+++ client/js/views/presentationSettings.js
@@ -0,0 +1,49 @@
+'use strict';
+
+var $ = require('jquery')
+  , request = require('superagent')
+
+
+var getSlideshowSettings = function() {
+
+  var settings = {};
+  [].slice.call(document.querySelectorAll('.slideshow-setting'))
+    .forEach(function(setting, index){
+
+    settings[setting.name] = setting.value;
+  });
+  return settings;
+}
+
+module.exports = {
+
+
+  init: function(){
+
+    // Prevent all forms from submission.
+    [].slice.call(document.querySelectorAll('form'))
+      .forEach(function(form, index){
+      form.addEventListener('submit', function(event) {
+        event.preventDefault();
+      })  
+    });
+
+    document.querySelector('#slideshowSettingsSave')
+      .addEventListener('click', function() {
+
+      request
+        .put('settings')
+        .send(getSlideshowSettings())
+        .set('Accept', 'application/json')
+        .end(function(err, res){
+          // handle error
+          if(err || res.statusType!=2){
+            alert('Something went wrong with removing your presentation: ' + 
+              (err!=null ? err.message : JSON.stringify(res.body)));
+            return;
+          }
+          console.log(res);
+        });
+    });
+  }
+}
