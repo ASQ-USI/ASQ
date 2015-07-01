77e21ca035fd0aaf6f1bb0ce8f16ff83a7d049e7
ZhenfeiNie <zhenfei.nie@gmail.com>
2015-07-01 11:45:55 +0200

    models/exercise: using new setting model

--- models/exercise.js
+++ models/exercise.js
@@ -6,29 +6,35 @@
 var mongoose         = require('mongoose')
   , Schema           = mongoose.Schema
   , ObjectId         = Schema.ObjectId
-  , assessmentTypes = require('./assessmentTypes')
+  , Promise    = require("bluebird")
+  , coroutine  = Promise.coroutine
+  , assessmentTypes  = require('./assessmentTypes')
   , appLogger        = require('../lib/logger').appLogger;
+  Setting            = db.model('Setting');
 
 var exerciseSchema = new Schema({
   questions         : { type: [{ type: ObjectId, ref: 'Question' }], default: [] },
   assessmentTypes   : { type: [{ type: String, enum: assessmentTypes }], default: [] },
-  maxNumSubmissions : { type: Number, default: 1 },
-  confidence        : { type: Boolean, default: false }
+  settings          : { type: [{ type: ObjectId, ref: 'Setting' }], default: [] },
 });
 
 
 exerciseSchema.virtual('allowResubmit').get(function allowResubmit() {
   return this.assessmentTypes.indexOf('self') === -1 &&
     this.assessmentTypes.indexOf('peer') > -1 && this.resubmit;
-    
-  // if (this.assessmentTypes.indexOf('self') === -1 && 
-  //   this.assessmentTypes.indexOf('peer') > -1) {
-  //   return this.maxNumSubmissions
-  // }
 })
 
 exerciseSchema.set('toObject', { virtuals: true });
 exerciseSchema.set('toJSON', { virtuals: true });
+
+exerciseSchema.methods.getSettings = coroutine(function* getSettingsGen() {
+  var settings = [];
+  for ( var i=0; i<this.settings.length; i++ ) {
+    var tmp = yield Setting.findById(this.settings[i]).exec();
+    settings.push(tmp);
+  }
+  return settings;
+});
 
 appLogger.debug('Loading Exercise model');
 mongoose.model('Exercise', exerciseSchema);
