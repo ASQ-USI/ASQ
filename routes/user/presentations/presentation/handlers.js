ed396504a9bf0df7b8dc0dc9c7eff565a112b704
ZhenfeiNie <zhenfei.nie@gmail.com>
2015-07-01 18:21:35 +0200

    routes/user/presentations/presentation/handlers: getPresentationSettings: clean up

--- routes/user/presentations/presentation/handlers.js
+++ routes/user/presentations/presentation/handlers.js
@@ -334,7 +334,7 @@ var getPresentationSettings = coroutine(function* getPresentationSettings(req, r
     return res.render('404', {'msg': 'Presentation not found'});
   }
 
-  var presentationSettings = yield presSettings.getDustifySettings(slideshow);
+  var presentationSettings = yield slideshow.getSettings();
   var exerciseSettings = yield presSettings.getDustifySettingsOfExercisesAll(slideshow);
 
   // Whether the slideshow is currently active(running) by this user
@@ -355,7 +355,7 @@ var getPresentationSettings = coroutine(function* getPresentationSettings(req, r
 });
 
 var putPresentationSettings = coroutine(function* putPresentationSettings(req, res) {
-  console.log(req.body);
+  console.log(req.body, 3);
   var state = yield Conf.updateSlideshowConf(req.body, req.params.presentationId);
   res.send(state);
 });
