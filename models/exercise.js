bad6b7b483ec3aa64c8192b502a93556baeb0ed6
ZhenfeiNie <zhenfei.nie@gmail.com>
2015-07-09 18:58:39 +0200

    models/exercise: add util method

--- models/exercise.js
+++ models/exercise.js
@@ -21,10 +21,24 @@ var exerciseSchema = new Schema({
 });
 
 
-exerciseSchema.virtual('allowResubmit').get(function allowResubmit() {
-  return this.assessmentTypes.indexOf('self') === -1 &&
-    this.assessmentTypes.indexOf('peer') > -1 && this.resubmit;
-})
+// exerciseSchema.virtual('allowResubmit').get(coroutine(function* allowResubmitGen() {
+//   var max = yield this.getSetting('maxNumSubmissions');
+
+//   // max < 0 means infinite submissions allowed
+//   if( max < 0) return Promise.resolve(true);
+
+//   var count = yield ExerciseSubmission.where({
+//     'exercise': submission.exerciseUid,
+//     'answeree': submission.answeree,
+//     'session' : submission.session,
+//   }).count();
+
+//   return Promise.resolve(count < max);
+// }));
+// 
+exerciseSchema.virtual('allowResubmit').get(function allowResubmit(){
+  return true;
+});
 
 exerciseSchema.set('toObject', { virtuals: true });
 exerciseSchema.set('toJSON', { virtuals: true });
@@ -37,6 +51,15 @@ exerciseSchema.methods.getSettings = coroutine(function* getSettingsGen() {
     settings.push(tmp);
   }
   return settings;
+});
+
+exerciseSchema.methods.getSetting = coroutine(function* getSetttingGen(key) {
+  for ( var i=0; i<this.settings.length; i++ ) {
+    var setting = yield Setting.findById(this.settings[i]).exec();
+    if ( setting.key === key ) {
+      return setting.value;
+    }
+  }
 });
 
 logger.debug('Loading Exercise model');
