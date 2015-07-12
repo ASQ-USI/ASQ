74d054adba035b7fd1db152ea26958b8af0eb26e
ZhenfeiNie <zhenfei.nie@gmail.com>
2015-07-12 14:05:15 +0200

    rename event to `asq:update_live_presentation_settings` and move its  handler to asq-exercise

--- client/js/presenter.js
+++ client/js/presenter.js
@@ -95,7 +95,7 @@ this.connect = function(){
     "asq:stat",
     "asq:question_type",
     "asq:session-terminated",
-    'setting:update-live-presentation-settings'
+    'asq:update_live_presentation_settings'
   ];
   connection.addEvents2Forward(events2Forward);
   connection.connect(this.protocol, this.host, this.port, this.sessionId, this.namespace, this.token, eventBus);
@@ -136,34 +136,10 @@ this.subscribeToEvents= function (){
   .on('asq:question_type', function(evt){
     console.log(evt);
     debug.log(evt)
-  })
-  .on('setting:update-live-presentation-settings', updateLivePresentationSettings);
-}
-
-function updateExerciseSettings(exercise, settings) {
-  Object.keys(settings).forEach(function(key, index){
-    exercise.setAttribute(key.toLowerCase(), settings[key]);
-    exercise[key] = settings[key];
   });
 }
 
-function updateLivePresentationSettings(evt) {
-  debug.log('updateLivePresentationSettings', evt);
 
-  if ( evt.scope == 'exercise' ) {
-    var selector = 'asq-exercise[uid="' + evt.exerciseId + '"]';
-    var exercise = document.querySelector(selector);
-    if ( !exercise ) return;
-    updateExerciseSettings(exercise, evt.settings);
-  } 
-  else if ( evt.scope == 'presentation' ) {
-    [].slice.call(document.querySelectorAll('asq-exercise'))
-      .forEach(function(exercise, index){
-        
-      updateExerciseSettings(exercise, evt.settings);
-    });
-  }
-}
 
 // querySelectorAll2Array :-)
 function qSA2Ar(s){
