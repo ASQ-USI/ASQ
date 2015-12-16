57853a7935e12690feb005bd8cd98352828b28b5
Zhenfei Nie <youyis2fox@gmail.com>
2015-12-16 23:50:16 +0100

    first version of asq-fork-reveal

--- client/js/presentationAdapter/adapters.js
+++ client/js/presentationAdapter/adapters.js
@@ -2,5 +2,9 @@
 // of the required module
 module.exports = {
   "impress" : require('impress-asq-adapter'),
-  "impressAsqFork" : require('impress-asq-fork-asq-adapter')
+  "impressAsqFork" : require('impress-asq-fork-asq-adapter'),
+  'revealAsqFork' : require('../reveal-asq-fork-asq-adapter.js')
+
+
+
 }
