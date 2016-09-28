253de27ce322fc0f9c75819326a98a8fcd9c6c09
Vasileios Triglianos <triglian@gmail.com>
2016-09-29 01:29:14 +0200

    refactor

---
+++ test/unit/utils/fs.test.js
@@ -0,0 +1,11 @@
+describe('lib/utils/fs', function() {
+
+  describe('.getExtension(filename)', function(){
+    it.skip('should be tested');
+  });
+
+  describe('.getFirstHtmlFile(path)', function(){
+    it.skip('should be tested');
+  });
+
+});
