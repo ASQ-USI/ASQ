762d1e000d358a7d60769b94152e565df89dc639
Vasileios Triglianos <triglian@gmail.com>
2016-11-13 22:42:24 +0100

    rollback nginx conf

--- lib/support/docker/runit/nginx.sh
+++ lib/support/docker/runit/nginx.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 #Configure nginx
-client_max_body_size="NGINX_CLIENT_MAX_BODY_SIZE"
-client_max_body_size_val="${!client_max_body_size}"
-		
-if [ "$client_max_body_size_val" ]; then
-	sed -ri 's/(client_max_body_size)\s+(\d+[M,m]?;)/\1 '$client_max_body_size_val';/' /etc/nginx/nginx.conf
-fi
-
 nginx 2>&1
