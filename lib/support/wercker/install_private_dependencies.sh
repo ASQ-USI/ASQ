0bc4b7a7a6e446780039df910b8e61d2ab2b8043
Vincenzo Ferme <fermevincenzo@gmail.com>
2016-11-29 21:43:23 +0100

    Cleans up git folder after installing private dependencies

--- lib/support/wercker/install_private_dependencies.sh
+++ lib/support/wercker/install_private_dependencies.sh
@@ -15,7 +15,11 @@ export cockpitDir=public/cockpit
 export cockpitBranch=feat-use-polymer-build
 export cockpitCommit=1cace88d4dff32e3366c1b6c97a3c4b350a6d81f
 
+# Execute the pipeline
 current_folder=$(pwd)
 cd ${WERCKER_SOURCE_DIR}
 CI_USER_TOKEN=${CI_USER_TOKEN} npm run install-private-dependencies
 cd $current_folder
+
+# Clean up folders
+rm -Rf $slide2htmlDir/.git $uiDir/.git $cockpitDir/.git
