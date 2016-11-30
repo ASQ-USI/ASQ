c5d22cc659b341b9deef251892809c64bd79d7c6
Vincenzo Ferme <fermevincenzo@gmail.com>
2016-11-30 23:52:14 +0100

    Fixes a typo using two _

--- lib/support/wercker/bower_install_with_cache.sh
+++ lib/support/wercker/bower_install_with_cache.sh
@@ -2,21 +2,21 @@
 # HANDLES CACHE FOR BOWER DEPENDENCIES
 
 # Partially inspired by: https://bitbucket.org/ryanharper007/wercker-bower-install/src/f6bc2d45546382012668ea8224f3f8bb6e743894/run.sh?at=master&fileviewer=file-view-default
-export BOWER_STORAGE__CACHE="${WERCKER_CACHE_DIR}/wercker/bower"
-mkdir -p $BOWER_STORAGE__CACHE
-mkdir -p $BOWER_STORAGE__CACHE/.bower-cache
-mkdir -p $BOWER_STORAGE__CACHE/.bower-registry
-mkdir -p $BOWER_STORAGE__CACHE/.bower-tmp
+export BOWER_STORAGE_CACHE="${WERCKER_CACHE_DIR}/wercker/bower"
+mkdir -p $BOWER_STORAGE_CACHE
+mkdir -p $BOWER_STORAGE_CACHE/.bower-cache
+mkdir -p $BOWER_STORAGE_CACHE/.bower-registry
+mkdir -p $BOWER_STORAGE_CACHE/.bower-tmp
 
 set +e
 bower install \
     --config.interactive=false \
-    --config.storage.packages=$BOWER_STORAGE__CACHE/.bower-cache \
-    --config.storage.registry=$BOWER_STORAGE__CACHE/.bower-registry \
-    --config.tmp=$BOWER_STORAGE__CACHE/.bower-tmp \
+    --config.storage.packages=$BOWER_STORAGE_CACHE/.bower-cache \
+    --config.storage.registry=$BOWER_STORAGE_CACHE/.bower-registry \
+    --config.tmp=$BOWER_STORAGE_CACHE/.bower-tmp \
     --allow-root
 result="$?"
-set -e
+set -e 
 
 # Fail if it is not a success or warning
 if [[ result -ne 0 && result -ne 6 ]]
