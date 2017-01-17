cf82bb2dfb88a63da1e77aebc8c9783aded67ed7
Emrah Signorini <emrah.signorini@usi.ch>
2017-01-17 16:16:22 +0100

    added upvote/downvote initial logic and with update on the database

--- models/answer.js
+++ models/answer.js
@@ -31,7 +31,9 @@ const answerSchema = new Schema({
   // submission : [],
   submission : { type: Schema.Types.Mixed },
   confidence : { type: Number, min: 0, max: 5, default: 0 }, // 0 = not set
-  logData    : [answerLogSchema]
+  logData    : [answerLogSchema],
+  upvotes    : { type: [{ type: ObjectId, ref: 'WhitelistEntry' }], default: [] },
+  downvotes  : { type: [{ type: ObjectId, ref: 'WhitelistEntry' }], default: [] }
 });
 
 answerSchema.index({ session: 1, answeree: 1, exercise: 1, submitDate: 1 });
@@ -46,7 +48,7 @@ answerSchema.post('save', function(answer){
       answer: answer._id
     }
   })
-})
+});
 
 
 // Saves an automatic assessment for a user submited answer asynchronously
