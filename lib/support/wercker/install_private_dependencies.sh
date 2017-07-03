cc82aea4fa3377bbedb00d816e007893e72516b6
Vincenzo Ferme <VincenzoFerme@users.noreply.github.com>
2017-07-03 18:04:35 +0200

    Switches the cockpit to the master branch

--- lib/support/wercker/install_private_dependencies.sh
+++ lib/support/wercker/install_private_dependencies.sh
@@ -13,8 +13,8 @@ export uiBranch=master
 export uiCommit=94d3b15481b416a987553adfbe8a3af99587cdb5
 
 export cockpitDir=public/cockpit
-export cockpitBranch=feat-use-polymer-build
-export cockpitCommit=1cace88d4dff32e3366c1b6c97a3c4b350a6d81f
+export cockpitBranch=master
+export cockpitCommit=c4052a007fd4dc366540f0f14ddfb44bcf710e2c
 
 # Execute the pipeline
 current_folder=$(pwd)
