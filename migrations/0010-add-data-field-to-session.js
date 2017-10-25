f3017aaf90edaca665e4e59ff54bd0f55b7f9d21
Vassilis Triglianos <triglian@gmail.com>
2017-10-25 11:01:30 +0200

    🐛 fix undefined data property for session

---
+++ migrations/0010-add-data-field-to-session.js
@@ -0,0 +1,22 @@
+
+const mongodb = require('mongodb');
+
+exports.up = function(db, next){
+    db.collection('sessions').update({
+      'data' : {$exists : false}
+    }, {
+      $set: {
+        'data' : {
+          'activeViewerQuestion': null,
+          'questions': [],
+          'studentQuestionsEnabled': false,
+        }
+      }
+    }, {
+      multi: true
+    }, next);
+};
+
+exports.down = function(db, next){
+    next();
+};
