4eab123c18e174d15eee48c41fda8817055fc29b
Vassilis Triglianos <triglian@gmail.com>
2017-10-25 15:35:45 +0200

    fix identation

--- migrations/0010-add-data-field-to-session.js
+++ migrations/0010-add-data-field-to-session.js
@@ -2,21 +2,21 @@
 const mongodb = require('mongodb');
 
 exports.up = function(db, next){
-    db.collection('sessions').update({
-      'data' : {$exists : false}
-    }, {
-      $set: {
-        'data' : {
-          'activeViewerQuestion': null,
-          'questions': [],
-          'studentQuestionsEnabled': false,
-        }
+  db.collection('sessions').update({
+    'data' : {$exists : false}
+  }, {
+    $set: {
+      'data' : {
+        'activeViewerQuestion': null,
+        'questions': [],
+        'studentQuestionsEnabled': false,
       }
-    }, {
-      multi: true
-    }, next);
+    }
+  }, {
+    multi: true
+  }, next);
 };
 
 exports.down = function(db, next){
-    next();
+  next();
 };
